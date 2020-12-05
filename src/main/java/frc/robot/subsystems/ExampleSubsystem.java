f9c75a6a49f34144bc5deb7270a957a2c462956c
TMeredith22 <tmeredith22@bfhsla.org>
2020-12-05 18:46:13 +0000

    First Commit
    Signed-off-by: TMeredith22 <tmeredith22@bfhsla.org>

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -14,8 +14,6 @@ public class ExampleSubsystem extends SubsystemBase {
    * Creates a new ExampleSubsystem.
    */
   public ExampleSubsystem() {
-
-    //Working out some kinks
   }
 
   @Override
